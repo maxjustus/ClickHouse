1511447f5a8e45ce2da45dd8e52ed717c3a60da9
Max Justus Spransy <maxjustus@gmail.com>
2026-03-30 11:45:51 -0700

    chore: disable alias identifier cache inside subqueries
    It's less optimal and the approach this removes should work and passes
    tests, but disabling cache inside subqueries is more conservative which
    makes some sense for the initial introduction of this cache.

--- src/Analyzer/Resolve/QueryAnalyzer.cpp
+++ src/Analyzer/Resolve/QueryAnalyzer.cpp
@@ -1355,10 +1355,9 @@ IdentifierResolveResult QueryAnalyzer::tryResolveIdentifier(const IdentifierLook
     IdentifierResolveScope & scope,
     IdentifierResolveContext identifier_resolve_context)
 {
-    IdentifierLookup cache_lookup = identifier_lookup;
-    cache_lookup.subquery_function_instance_id = scope.expressions_in_resolve_process_stack.getSubqueryFunctionInstanceId();
+    const bool inside_subquery_function = scope.expressions_in_resolve_process_stack.isInsideSubqueryFunction();
 
-    auto it = scope.identifier_in_lookup_process.find(cache_lookup);
+    auto it = scope.identifier_in_lookup_process.find(identifier_lookup);
 
     bool already_in_resolve_process = false;
     if (it != scope.identifier_in_lookup_process.end())
@@ -1368,11 +1367,14 @@ IdentifierResolveResult QueryAnalyzer::tryResolveIdentifier(const IdentifierLook
     }
     else
     {
-        auto cached_result = scope.findCachedIdentifier(cache_lookup, identifier_resolve_context);
-        if (cached_result)
-            return *cached_result;
+        if (!inside_subquery_function)
+        {
+            auto cached_result = scope.findCachedIdentifier(identifier_lookup, identifier_resolve_context);
+            if (cached_result)
+                return *cached_result;
+        }
 
-        auto [insert_it, _] = scope.identifier_in_lookup_process.insert({cache_lookup, IdentifierResolveState()});
+        auto [insert_it, _] = scope.identifier_in_lookup_process.insert({identifier_lookup, IdentifierResolveState()});
         it = insert_it;
     }
 
@@ -1485,8 +1487,8 @@ IdentifierResolveResult QueryAnalyzer::tryResolveIdentifier(const IdentifierLook
     if (it->second.count == 0)
     {
         scope.identifier_in_lookup_process.erase(it);
-        if (resolve_result.resolved_identifier)
-            scope.tryCacheIdentifier(cache_lookup, resolve_result, identifier_resolve_context);
+        if (resolve_result.resolved_identifier && !inside_subquery_function)
+            scope.tryCacheIdentifier(identifier_lookup, resolve_result, identifier_resolve_context);
     }
 
     return resolve_result;
