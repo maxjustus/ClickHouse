e634d76c7bc2d5039eb98cd0d44b83f98747ba68
Max Justus Spransy <maxjustus@gmail.com>
2026-04-22 20:11:48 -0700

    fix: remove rogue newline

--- src/Analyzer/Utils.h
+++ src/Analyzer/Utils.h
@@ -129,7 +129,6 @@ void assertNoFunctionNodes(const QueryTreeNodePtr & node,
   */
 bool hasFunctionNode(const QueryTreeNodePtr & node, std::string_view function_name);
 
-
 /** Replace columns in node children.
   * If there is column node and its source is specified table expression node and there is
   * node for column name in map replace column node with node from map.
